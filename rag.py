703bf0e7a03318e08bb71cb93a99eaa78cfe7252
AIML-Engineer1 <rajsrinit@gmail.com>
2025-08-29 11:27:40 -0700

    Update rag.py

--- rag.py
+++ rag.py
@@ -16,7 +16,7 @@ os.environ["PINECONE_API_KEY"] = st.secrets["llm"]["PINECONE_API_KEY"]
 os.environ["INDEX_HOST"] = st.secrets["llm"]["INDEX_HOST"]
 
 # constants
-NAMESPACE_KEY = "str"
+NAMESPACE_KEY = "sri"
 TEXT_MODEL = "text-embedding-ada-002"
 QA_MODEL = "gpt-4o-mini"
 COMMON_ENGLISH_TEMPLATE = """
